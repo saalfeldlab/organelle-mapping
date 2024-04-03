a134b4e3608ebab76e971c4934c69fd3972623df
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-04-03 13:47:40 -0400

    feat: :sparkles: turn offset fixing from a notebook into a cli

--- src/fly_organelles/data_preparation.py
+++ src/fly_organelles/data_preparation.py
@@ -2,10 +2,16 @@ import zarr
 import numpy as np
 import logging
 import numcodecs
+import yaml
+import click
+import fibsem_tools as fst
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+@click.group()
+def cli():
+    pass
 
 def create_label(crop_full_path: str, new_label_name: str, atomic_labels: list[str]):
     logger.info(f"Exporting {new_label_name} in {crop_full_path} by combining {atomic_labels}")
@@ -95,6 +101,96 @@ def create_label(crop_full_path: str, new_label_name: str, atomic_labels: list[s
         crop.attrs.refresh()
         logger.info(f"check new class list: {crop.attrs['cellmap']['annotation']['class_names']}")
 
+def corner_offset(center_off_arr, raw_res_arr, crop_res_arr):
+    return (center_off_arr + raw_res_arr/2. - crop_res_arr/2.)
 
-if __name__ == "__main__":
-    crop_path = "/nrs/saalfeld/heinrichl/data/cellmap_labels/fly_organelles/jrc_mb-1a/groundtruth.zarr/crop120"
+def valid_offset(center_off_arr, raw_res_arr, crop_res_arr):
+    corner_off_arr = corner_offset(center_off_arr, raw_res_arr, crop_res_arr)
+    return np.all(corner_off_arr % raw_res_arr == 0) and np.all(corner_off_arr % crop_res_arr == 0)
+
+@cli.command()
+@click.argument("crop_path", type=str)
+@click.option("--orig_offset", type=float, nargs=3)   
+@click.option("--new_offset", type=float, nargs=3) 
+@click.option("-r", "--round", is_flag=True)
+def edit_offset(crop_path, orig_offset, new_offset, round: bool=False):
+    edit_offset_func(crop_path, orig_offset, new_offset, round=round)
+
+def edit_offset_func(crop_path, orig_offset, new_offset, round: bool=False):
+    diff_offset = np.array(orig_offset) - np.array(new_offset)
+    crop_root = fst.access(crop_path, mode="r+")
+    for lbl in list(crop_root.keys()):
+        crop_label_root = crop_root[lbl]
+        ms_attrs = crop_label_root.attrs["multiscales"]
+        for ds in range(len(ms_attrs[0]["datasets"])):
+            for k in range(len(ms_attrs[0]["datasets"][ds]["coordinateTransformations"])):
+                if ms_attrs[0]["datasets"][ds]["coordinateTransformations"][k]["type"] == "translation":
+                    transl = ms_attrs[0]["datasets"][ds]["coordinateTransformations"][k]["translation"] 
+                    set_offset = np.array(transl) - diff_offset
+                    if round:
+                        set_offset = np.round(set_offset)    
+                    ms_attrs[0]["datasets"][ds]["coordinateTransformations"][k]["translation"] = list(set_offset)
+        crop_label_root.attrs["multiscales"] = ms_attrs
+
+@cli.command()
+@click.argument("data-config", type=click.File("rb"))
+@click.option("-n", "--dry-run", is_flag=True)
+@click.option("--gt_path", type=str, help="path to groundtruth crops", default="/nrs/saalfeld/heinrichl/data/cellmap_labels/fly_organelles/", show_default=True)
+def fix_offset(data_config, gt_path: str = "/nrs/saalfeld/heinrichl/data/cellmap_labels/fly_organelles/", dry_run: bool = False):
+    datas = yaml.safe_load(data_config)
+    summary = {
+        "valid": [], 
+        "invalid": []
+    }
+    if dry_run:
+        summary["correctable"] = []
+    else:
+        summary["corrected"] = []
+    for key, ds_info in datas.items():
+        logger.info(f"Processing {key}")
+        root_raw = fst.read(ds_info["raw"])
+        try: 
+            datasets = root_raw.attrs["multiscales"][0]["datasets"]
+        except KeyError as e:
+            logger.info("May have different metadata format. Proceeding with next dataset")
+            continue
+        for ds in datasets:
+            if ds["path"] == "s0":
+                raw_res = ds["coordinateTransformations"][0]["scale"]
+        raw_res_arr = np.array(raw_res)
+        for crop in ds_info["crops"]:
+            print(f"{gt_path}{key}/groundtruth.zarr/{crop}")
+            crop_path = f"{gt_path}{key}/groundtruth.zarr/{crop}"
+            crop_root = fst.read(crop_path)
+            lbl = list(crop_root.keys())[0]
+            crop_datasets = crop_root[lbl].attrs["multiscales"][0]["datasets"]
+            for cds in crop_datasets:
+                if cds["path"] == "s0":
+                    crop_res = cds["coordinateTransformations"][0]["scale"]
+                    crop_res_arr = np.array(crop_res)
+                    crop_off = cds["coordinateTransformations"][1]["translation"]
+                    crop_off_arr = np.array(crop_off)
+                    
+                    if valid_offset(crop_off_arr, raw_res_arr, crop_res_arr):
+                        logger.info(f"Crop {crop} passed with raw resolution: {raw_res}, crop resolution {crop_res} and crop offset {crop_off}")
+                        summary["valid"].append(crop)
+                    else:
+                        logger.info(f"Crop {crop} did not pass with raw resolution: {raw_res}, crop resolution {crop_res} and crop offset {crop_off}")
+                        if np.all(crop_res < raw_res):
+                            crop_off_arr_suggested = crop_off_arr - crop_res_arr/2.
+                            
+                        else:
+                            crop_off_arr_suggested = crop_off_arr + crop_res_arr /2. - raw_res_arr / 2.
+                        if valid_offset(crop_off_arr_suggested, raw_res_arr, crop_res_arr):
+                            logger.info(f" Crop {crop} can be automatically corrected with {crop_off_arr_suggested}.")
+                            if not dry_run:
+                                edit_offset_func(crop_path, crop_off, list(crop_off_arr_suggested))
+                                summary["corrected"].append(crop)
+                            else:
+                                summary["correctable"].append(crop)
+                        else:
+                            summary["invalid"].append(crop)
+                            logger.info(f"Crop {crop} cannot be automatically corrected.")                                
+    click.echo(summary)
+    return summary
+
